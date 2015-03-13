3544ea9438ac028481e012ff753f536d6eb3f2fd
will <will@will-ThinkPad-E420.(none)>
2015-03-13 08:27:39 +0800

    add work break

--- java/LeetCode/WordBreak.java
+++ java/LeetCode/WordBreak.java
@@ -1,7 +1,57 @@
+// Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+// For example, given
+// s = "leetcode",
+// dict = ["leet", "code"].
+// Return true because "leetcode" can be segmented as "leet code".
+
 import java.io.*;
+import java.util.*;
 
 public class WordBreak{
-  public static void main(String[] args){
-    System.out.print("111");
-  }
+
+	public static void main(String[] args){
+
+		String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+		Set<String> dict = new HashSet<String>();
+		dict.add("a");
+		dict.add("aa");
+		dict.add("aaa");
+		dict.add("aaaa");
+		dict.add("aaaaa");
+		if(wordBreak(s, dict)){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}		
+	}
+	
+	public static boolean wordBreak(String s, Set<String> dict) {
+		int len = s.length();
+		//null string
+		if(len==0) {return false;}
+		//single char string
+		if(len==1) {return match(s, dict);}
+		//multi char string
+		String part1,part2;
+		for(int i=0; i<len; i++){
+			//all char string
+			if(i==len-1 && match(s, dict)) {return true;}
+			part1=s.substring(0,i+1);
+			part2=s.substring(i+1,len);
+			if( match(part1, dict) && wordBreak(part2, dict)) {
+				return true;
+			} 
+		}
+		return false;		
+	}
+
+	public static boolean match(String s, Set<String> dict) {
+		for (String e: dict){
+			if(e.equals(s)){
+				return true;
+			}
+		}
+		return false;
+	}	
+
 }
---
+++ java/LeetCode/testSubstring.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class testSubstring{
+	public static void main(String[] args){
+		String s = "niuniu";
+		System.out.println(s.substring(6,6));	
+	}
+}
